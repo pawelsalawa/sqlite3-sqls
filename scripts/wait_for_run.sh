483824522c470a9646a7fe404d3e136c475c5863
Paweł Salawa <pawelsalawa@gmail.com>
2025-01-04 17:14:57 +0100

    Attempt to simplify workflow file and to move bash code into reusable files.

--- scripts/wait_for_run.sh
+++ scripts/wait_for_run.sh
@@ -10,7 +10,7 @@ resp=$(curl -s -L \
   https://api.github.com/repos/$REPO/actions/runs/$RUN_ID)
 
 
-echo "Waiting for ID: $RUN_ID" 2>&2
+echo "Waiting for ID: $RUN_ID" >&2
 success=0
 finished=0
 status=""
@@ -19,19 +19,21 @@ SECS=0
 INTERVAL=15
 while [ "$status" != "completed" ] && [ $SECS -le $TOTAL_SECS ] && [ $finished -eq 0 ]
 do
-    printf "Iteration %02d:%02d / %02d:%02d\n" $((SECS / 60)) $((SECS % 60)) $((TOTAL_SECS / 60)) $((TOTAL_SECS % 60)) >&2
+    printf "Iteration for $RUN_ID: %02d:%02d / %02d:%02d\n" $((SECS / 60)) $((SECS % 60)) $((TOTAL_SECS / 60)) $((TOTAL_SECS % 60)) >&2
     SECS=$((SECS + INTERVAL))
     
+    url=https://api.github.com/repos/$REPO/actions/runs/$RUN_ID
+    echo "Querying status: $url" >&2
     resp=$(
             curl -s -L \
               -H "Accept: application/vnd.github+json" \
               -H "Authorization: Bearer $TOKEN" \
               -H "X-GitHub-Api-Version: 2022-11-28" \
-              https://api.github.com/repos/$REPO/actions/runs/$RUN_ID
+              $url
         )
     status=$(echo $resp | jq -r '.status')
     result=$(echo $resp | jq -r '.conclusion')
-    echo "Status: $status, result: $result" >&2
+    echo "Status: $status, Result: $result" >&2
     if [ "$status" == "completed" ] && [ "$result" != "success" ]
     then
         echo "Run has failed: $RUN_ID"
