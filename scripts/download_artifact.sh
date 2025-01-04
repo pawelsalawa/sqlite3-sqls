c1f1c983e00c44ee2fde35c1c443d29ff2ce672f
Paweł Salawa <pawelsalawa@gmail.com>
2025-01-04 18:26:49 +0100

    Attempt to simplify workflow file and to move bash code into reusable files.

--- scripts/download_artifact.sh
+++ scripts/download_artifact.sh
@@ -9,5 +9,5 @@ URL=$(curl -s -L \
   https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/artifacts \
   | jq -r '.artifacts[0].archive_download_url')
     
-echo "Download URL for $rid: $URL" >&2
+echo "Download URL for $RUN_ID: $URL" >&2
 curl -O -J -s -L -H "Authorization: Bearer $TOKEN" $URL
--- scripts/upload_artifact.sh
+++ scripts/upload_artifact.sh
@@ -3,11 +3,13 @@
 RELEASE_ID=$1
 FILE=$2
 
+fname=(basename "$FILE")
+
 curl -s -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: application/octet-stream" \
-  https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$FILE \
+  https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$fname \
   --data-binary "@$FILE"
